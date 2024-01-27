a80e1681ebbaec59bb055a9fcf96c33c52b1f343
zh3ngxu <xanderxu1994@gmail.com>
2024-01-27 20:45:53 +0000

    add shell script to upload files to s3 in batch

---
+++ sales_data/upload_raw_files.sh
@@ -0,0 +1,11 @@
+aws s3 cp sales_2024-01-01.csv s3://$mybucket/2024/01/01/sales_2024-01-01.csv
+aws s3 cp sales_2024-01-01.csv s3://$mybucket/2024/01/02/sales_2024-01-02.csv
+aws s3 cp sales_2024-01-01.csv s3://$mybucket/2024/01/03/sales_2024-01-03.csv
+aws s3 cp sales_2024-01-01.csv s3://$mybucket/2024/01/04/sales_2024-01-04.csv
+aws s3 cp sales_2024-01-01.csv s3://$mybucket/2024/01/05/sales_2024-01-05.csv
+
+aws s3 cp product.csv s3://$mybucket/2024/01/01/product.csv
+aws s3 cp product.csv s3://$mybucket/2024/01/02/product.csv
+aws s3 cp product.csv s3://$mybucket/2024/01/03/product.csv
+aws s3 cp product.csv s3://$mybucket/2024/01/04/product.csv
+aws s3 cp product.csv s3://$mybucket/2024/01/05/product.csv
