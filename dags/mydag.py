f023582b2a13f66fc7e794d1c5e6c0be0fe002ae
zh3ngxu <xanderxu1994@gmail.com>
2024-01-29 21:11:14 +0000

    add EMR add steps tasks

--- dags/mydag.py
+++ dags/mydag.py
@@ -23,7 +23,31 @@ def _notify_vendor():
     """TODO: implement the email or slack function to notify vendor about the filem misssing"""
     print("sales missing")
 
-CLUSTER_ID = "j-N22ODM1R21PY"
+
+default_args = {
+    "owner": "data_engineer",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 1, 5),
+    "end_date": datetime(2024, 1, 6),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    # 'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
+}
+
+
+dag = DAG(
+    "mydag",
+    default_args=default_args,
+    description="DAG to list objects in an S3 bucket",
+    catchup=True,
+    schedule_interval="0 6 * * *",
+)
+
+
+s3_bucket = "wcddeb8-lab-airflow"
+
+CLUSTER_ID = "j-QNO2YACKJS77" # create a new EMR cluster in AWS management console or create w/ EmrCreateJobFlowOperator
 
 # this is just an example of how to use SPARK_STEPS, you need to define your own steps
 SPARK_STEPS = [
@@ -46,34 +70,15 @@ SPARK_STEPS = [
             #    "3g",
             #    "--executor-cores",
             #    "2",
-               "s3://wcddeb8-lab-airflow/sales.py", "{{  macros.ds_format(ds,'%Y-%m-%d','%Y%m%d')  }}",
+               "s3://wcddeb8-lab-airflow/pyspark_sales.py",
+               "{{  macros.ds_format(ds,'%Y-%m-%d','%Y%m%d')  }}",
+               f"s3://{s3_bucket}/"+"{{ macros.ds_format(ds, '%Y-%m-%d', '%Y/%m/%d') }}/"
            ], 
        },
    }
 ]
 
-default_args = {
-    "owner": "data_engineer",
-    "depends_on_past": False,
-    "start_date": datetime(2024, 1, 1),
-    "end_date": datetime(2024, 1, 2),
-    "email_on_failure": False,
-    "email_on_retry": False,
-    # 'retries': 1,
-    # 'retry_delay': timedelta(minutes=5),
-}
 
-
-dag = DAG(
-    "mydag",
-    default_args=default_args,
-    description="DAG to list objects in an S3 bucket",
-    catchup=True,
-    schedule_interval="0 6 * * *",
-)
-
-
-s3_bucket = "wcddeb8-lab-airflow"
 
 
 list_objects_task = S3ListOperator(
@@ -103,42 +108,78 @@ emr_process_file = BashOperator(
     dag=dag
 )
 
-# Step 1: Create EMR Cluster
-create_emr_cluster = EmrCreateJobFlowOperator(
-    task_id="create_emr_cluster",
-    # ... EMR cluster configuration ...
-    aws_conn_id="aws_default",
-    emr_conn_id="emr_conn",
-    region_name="us-east-1",
+# Create EMR Cluster use below template to create EMR
+# create_emr_cluster = EmrCreateJobFlowOperator(
+#     task_id="create_emr_cluster",
+#     job_flow_overrides={
+#         "Name": "MyEMRCluster",
+#         "ReleaseLabel": "emr-6.3.0",
+#         "Applications": [{"Name": "Spark"}],
+#         "Instances": {
+#             'InstanceGroups': [
+#             {
+#                 "InstanceCount":1,
+#                 "InstanceGroupType":"MASTER",
+#                 "Name":"Primary",
+#                 "InstanceType":"m5.xlarge",
+#                 "EbsConfiguration":{
+#                     "EbsBlockDeviceConfigs":[
+#                         {
+#                         "VolumeSpecification":{
+#                             "VolumeType":"gp2",
+#                             "SizeInGB":32
+#                         },
+#                         "VolumesPerInstance":2
+#                         }
+#                     ]
+#                 }
+#             },
+#             {
+#                 "InstanceCount":1,
+#                 "InstanceGroupType":"CORE",
+#                 "Name":"Core",
+#                 "InstanceType":"m5.xlarge",
+#                 "EbsConfiguration":{
+#                     "EbsBlockDeviceConfigs":[
+#                         {
+#                         "VolumeSpecification":{
+#                             "VolumeType":"gp2",
+#                             "SizeInGB":32
+#                         },
+#                         "VolumesPerInstance":2
+#                         }
+#                     ]
+#                 }
+#             }
+#             ],
+#             'KeepJobFlowAliveWhenNoSteps': False,
+#             'TerminationProtected': False,
+#             'Ec2SubnetId': '<AWS_Subnet_ID>',
+#             "Ec2KeyName": "tgam-laptop",
+#         },
+#     },
+#     aws_conn_id="aws_default",
+#     emr_conn_id="emr_conn",
+#     region_name="us-east-1",
+#     dag=dag,
+# )
+
+# Add and run first Spark Jar step
+add_step_job = EmrAddStepsOperator(
+    task_id="add_step_job",
+    # job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster')['JobFlowId'] }}",
+    job_flow_id=CLUSTER_ID,
+    steps=SPARK_STEPS,
     dag=dag,
 )
 
-# Step 2: Add and run first Spark Jar step
-spark_step_1 = {
-    "Name": "sales processing",
-    "ActionOnFailure": "CONTINUE",
-    "HadoopJarStep": {
-        "Jar": "command-runner.jar",
-        "Args": ["spark-submit", "s3://your-bucket/spark-app-1.jar"]
-    }
-}
-add_step_1 = EmrAddStepsOperator(
-    task_id="add_step_1",
-    job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster')['JobFlowId'] }}",
-    steps=[spark_step_1],
+# Wait for first Spark Jar step to complete
+wait_for_step_job = EmrStepSensor(
+    task_id="wait_for_step_job",
+    # job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster')['JobFlowId'] }}",
+    job_flow_id=CLUSTER_ID,
+    step_id="{{ task_instance.xcom_pull(task_ids='add_step_job')[0] }}",
     aws_conn_id="aws_default",
-    emr_conn_id="emr_default",
-    region_name="us-west-2",
-    dag=dag,
-)
-
-# Step 3: Wait for first Spark Jar step to complete
-wait_for_step_1 = EmrStepSensor(
-    task_id="wait_for_step_1",
-    job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster')['JobFlowId'] }}",
-    step_id="{{ task_instance.xcom_pull(task_ids='add_step_1')['StepIds'][0] }}",
-    aws_conn_id="aws_default",
-    region_name="us-west-2",
     dag=dag,
 )
 
@@ -146,5 +187,12 @@ wait_for_step_1 = EmrStepSensor(
     list_objects_task
     >> check_s3_file_exist
     >> [notify_vendor,emr_process_file]
+)
+
+(
+    emr_process_file
+    # >> create_emr_cluster
+    >> add_step_job
+    >> wait_for_step_job
 )
 
