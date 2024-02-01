25d004c63aa86b5ca8038a59c3ed6dd4cb5a8f61
zh3ngxu <xanderxu1994@gmail.com>
2024-02-01 01:33:36 +0000

    reformat code

--- scripts/pyspark_sales.py
+++ scripts/pyspark_sales.py
@@ -5,30 +5,24 @@ from pyspark.sql import SparkSession
 date_str = sys.argv[1]
 file_input = sys.argv[2]
 
-# Create SparkSession 
-spark = SparkSession.builder \
-      .master("yarn") \
-      .appName("demo") \
-      .getOrCreate() 
+# Create SparkSession
+spark = SparkSession.builder.master("yarn").appName("demo").getOrCreate()
 
 sales_df = (
-    spark.read
-        .option("header","true")
-        .option("delimiter",",")
-        .csv(f"{file_input}")
+    spark.read.option("header", "true").option("delimiter", ",").csv(f"{file_input}")
 )
 
 sales_df.createOrReplaceTempView("sales")
 
-df_sum_sales_qty = spark.sql('select trans_dt,store_key,prod_key,sum(sales_qty) as sum_sales_qty from sales group by 1,2,3;')
+df_sum_sales_qty = spark.sql(
+    "select trans_dt,store_key,prod_key,sum(sales_qty) as sum_sales_qty from sales group by 1,2,3;"
+)
 
 df_sum_sales_qty.show(2)
 
 (
-    df_sum_sales_qty
-        .repartition(1)
-        .write
-        .mode("overwrite")
-        .option("compression","gzip")
-        .parquet(f"s3://wcddeb8-lab-airflow/output/sales_date={date_str}")
+    df_sum_sales_qty.repartition(1)
+    .write.mode("overwrite")
+    .option("compression", "gzip")
+    .parquet(f"s3://wcddeb8-lab-airflow/output/sales_date={date_str}")
 )
